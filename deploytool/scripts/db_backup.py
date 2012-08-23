45c1147c5f1f96eca3d0723e7dc88aa055a1b935
Dennis Bunskoek <dbunskoek@leukeleu.nl>
2012-08-23 23:42:43 +0200

    changed tabs to spaces, added newline at end of files

--- deploytool/scripts/db_backup.py
+++ deploytool/scripts/db_backup.py
@@ -5,10 +5,10 @@ from credentials import *
 
 
 command = 'mysqldump --user="%s" --password="%s" "%s" > %s' % (
-	username,
-	password,
-	database,
-	sys.argv[1]
+    username,
+    password,
+    database,
+    sys.argv[1]
 )
 
 subprocess.call(command, shell=True)
--- deploytool/scripts/sql_query.py
+++ deploytool/scripts/sql_query.py
@@ -5,10 +5,10 @@ from credentials import *
 
 
 command = 'mysql --user="%s" --password="%s" --database="%s" --execute="%s"' % (
-	username,
-	password,
-	database,
-	sys.argv[1]
+    username,
+    password,
+    database,
+    sys.argv[1]
 )
 
 subprocess.call(command, shell=True)
