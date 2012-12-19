6b5a784c9eb10c216ede4a7865375aeeac904d05
Marco Braak <mbraak@ridethepony.nl>
2012-12-19 10:30:59 +0100

    Only ask for mysql root password if necessary

--- deploytool/db/mysql.py
+++ deploytool/db/mysql.py
@@ -6,33 +6,36 @@ class DatabaseOperations(object):
     needs_password = True
     engine_name = 'mysql'
 
-    def __init__(self):
-        self.root_password = prompt(yellow('Password for mysql root user:'))
-
     def database_exists(self, database_name):
-        output = self.execute(
+        output = self.root_execute(
             "SHOW DATABASES LIKE \'%s\'" % database_name,
             '--skip-column-names'
         )
         return bool(output.strip().lower() == database_name.lower())
 
     def create_database(self, database_name, owner, password):
-        self.execute(r"CREATE DATABASE IF NOT EXISTS \\`%s\\` CHARACTER SET utf8 COLLATE utf8_general_ci" % database_name)
+        self.root_execute(r"CREATE DATABASE IF NOT EXISTS \\`%s\\` CHARACTER SET utf8 COLLATE utf8_general_ci" % database_name)
 
-        self.execute(
+        self.root_execute(
             r"GRANT ALL PRIVILEGES ON \\`%s\\`.* TO '%s'@'localhost' IDENTIFIED BY '%s' WITH GRANT OPTION" % (
                 database_name,
                 owner,
                 password
             )
         )
-        self.execute('FLUSH PRIVILEGES')
+        self.root_execute('FLUSH PRIVILEGES')
 
-    def execute(self, sql, options=''):
+    def root_execute(self, sql, options=''):
         return sudo(
             'mysql --batch --user=root --password=%s %s -e "%s"' % (
-                self.root_password,
+                self.get_root_password(),
                 options,
                 sql
             )
         )
+
+    def get_root_password(self):
+        if not hasattr(self, '_root_password'):
+            self._root_password = prompt(yellow('Password for mysql root user:'))
+
+        return self._root_password
