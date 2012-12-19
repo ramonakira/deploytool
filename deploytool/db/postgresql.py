2a6ab9fa5ec4bfb3978ad21a3f014ac3822df6b8
Marco Braak <mbraak@ridethepony.nl>
2012-12-19 10:51:18 +0100

    Create Postgresql database user

--- deploytool/db/postgresql.py
+++ deploytool/db/postgresql.py
@@ -10,13 +10,15 @@ class DatabaseOperations(object):
         return output == '1'
 
     def create_database(self, database_name, owner, password):
-        sudo(
-            'createdb %s -O %s' % (database_name, owner),
-            user='postgres'
-        )
+        if not self.database_exists(database_name):
+            self.sudo_postgres('createuser %s' % owner)
+            self.sudo_postgres('createdb %s -O %s' % (database_name, owner))
 
     def execute(self, sql):
         return sudo(
             'psql -t -A -c "%s"' % sql,
             user='postgres'
         )
+
+    def sudo_postgres(self, command):
+        return sudo(command, user='postgres')
