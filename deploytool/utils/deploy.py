62a473f7551548f98c63cea90498be2386ee37be
Marco Braak <mbraak@ridethepony.nl>
2014-08-13 13:48:22 +0200

    Updated gunicorn to 19.1.0

--- deploytool/utils/deploy.py
+++ deploytool/utils/deploy.py
@@ -186,7 +186,7 @@ class WebsiteDeployment(object):
         instance.pip_install_package(
             self.virtualenv_path,
             'gunicorn',
-            '19.0.0',
+            '19.1.0',
             env.cache_path,
             self.log_path,
         )
